85b7c4eb94644acf92f75457e2882baa0b1bdfd7
AshanSandeepa1 <ashansandeepa123@gmail.com>
2024-04-09 18:49:17 +0530

    transaction table updated

---
+++ Supermarket-Management-System/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_Management_System
+{
+    public class CartItem
+    {
+        public string Barcode { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get { return Quantity * UnitPrice; } }
+    }
+}
